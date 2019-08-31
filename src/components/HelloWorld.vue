65c822ec31e9cc3866c3f89aaa662116d1134e77
aleasper <alexidest@gmail.com>
2019-09-01 00:31:14 +0300

    Add print in console list of groups

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,18 +12,61 @@ import Vue from 'vue';
 import {   getGroups   } from '../api';
 import vuetify from '../plugins/vuetify'; // path to vuetify export
 import { Component } from 'vue-property-decorator';
+import { GroupInterface } from '../interfaces';
 
 @Component
 export default class GroupList extends Vue {
     public async getList() {
-        this.groups = await.getGroups();
-    }
-}
-
-export default {
+        let groups: Array<GroupInterface> = [];
+        groups = await getGroups();
+        console.log(groups)
+        for (let key in groups){
+            console.log("id: " + groups[key]['group_id'] + " group: " + groups[key]['number'])
+        }
+    };
     created() {
-        GroupList.getList();
-    },
+        this.getList();
+    };
+    data () {
+      return {
+        headers: [
+          {
+            text: 'avalible group number',
+            sortable: true,
+            value: number,
+          },
+          { text: 'group id', value: id },
+        ],
+        groups: [
+            // heed to create dynamic data table
+          {
+            group_num: '111',
+            id: 1,
+          },
+          {
+              group_num: '222',
+              id: 2,
+          },
+          {
+              group_num: '333',
+              id: 3,
+          },
+          {
+              group_num: '444',
+              id: 4,
+          },
+          {
+              group_num: '555',
+              id: 5,
+          },
+          //
+        ],
+      }
+  };
+}
+/*
+export default {
+
     data () {
       return {
         headers: [
@@ -61,6 +104,7 @@ export default {
       }
     },
 }
+*/
 
 /*
 //
