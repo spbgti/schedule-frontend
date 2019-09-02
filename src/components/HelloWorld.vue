53926e4edba4de020ecc31c55e7532e03c4398dc
aleasper <alexidest@gmail.com>
2019-09-02 20:33:15 +0300

    Fix data module of tables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-data-table
+<v-data-table
     :headers="headers"
     :items="groups"
     :items-per-page="15"
+    item-key="number"
+    show-select
     class="elevation-1"
-  ></v-data-table>
+></v-data-table>
+
 </template>
 
 <script lang="ts">
@@ -18,96 +21,26 @@ import { GroupInterface } from '../interfaces';
 export default class GroupList extends Vue {
 
     groups: Array<GroupInterface> = [];
-
+    headers : [{ text: string; sortable: boolean; value: string; }, { text: string; value: string; }] = [
+      {
+        text: 'avalible group number',
+        sortable: true,
+        value: 'number', // value = key of json
+      },
+      { text: 'group id', value: 'group_id' }, // value = key of json
+    ];
     public async getList() {
         this.groups = await getGroups();
-        // delete if is not necessari from <here>
+        /* // delete if is not necessari from <here>
         console.log(this.groups);
         for (let key in this.groups){
             console.log("id: " + this.groups[key]['group_id'] + " group: " + this.groups[key]['number']);
         }
-        // to <here>
+        */ // to <here>
     };
-    
+
     created() {
         this.getList();
     };
-
-    data () { // refers to <v-data-table>
-      return {
-          headers : [
-            {
-              text: 'avalible group number',
-              sortable: true,
-              value: 'number', // value = key of json
-            },
-            { text: 'group id', value: 'group_id' }, // value = key of json
-          ],
-          groups:this.groups,
-      }
-    };
 }
-
-
-
-/*
-export default {
-
-    data () {
-      return {
-        headers: [
-          {
-            text: 'avalible group number',
-            sortable: true,
-            value: 'group_num',
-          },
-          { text: 'group id', value: 'id' },
-        ],
-        groups: [
-            //
-          {
-            group_num: '111',
-            id: 1,
-          },
-          {
-              group_num: '222',
-              id: 2,
-          },
-          {
-              group_num: '333',
-              id: 3,
-          },
-          {
-              group_num: '444',
-              id: 4,
-          },
-          {
-              group_num: '555',
-              id: 5,
-          },
-          //
-        ],
-      }
-    },
-}
-*/
-
-/*
-//
-<v-app>
-   <v-content>
-       <v-container>
-           <!-- Test page components here -->
-       </v-container>
-   </v-content>
-</v-app>
-//
-export default Vue.extend({
-    data: () => ({
-    }),
-    created() {
-        getGroups();
-    }
-});
-*/
 </script>
