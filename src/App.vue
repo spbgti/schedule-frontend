04aae5a703c4a93860e9a9f762669b6b8520add4
aleasper <alexidest@gmail.com>
2019-10-12 00:12:10 +0300

    Fix modules name error

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-content>
-      <Schedule_mod/>
+      <Schedule/>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Schedule_mod from './components/Schedule_mod.vue';
+import Schedule from './components/Schedule.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    Schedule_mod,
+    Schedule,
   },
   data: () => ({
     //
